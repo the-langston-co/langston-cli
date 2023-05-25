8728b564efca0a21dec76650044e406a315fa578
Neil Poulin <neil@neilpoulin.com>
2023-05-24 19:02:46 -0600

    update bundle script

--- bundle.sh
+++ bundle.sh
@@ -13,6 +13,8 @@ if [ -z "$PROPOSED_VERSION" ]; then
 elif [[ "$PROPOSED_VERSION" =~ ${VERSION_REGEX} ]]; then
   echo "Setting version to \"$PROPOSED_VERSION\""
   VERSION=$PROPOSED_VERSION
+  git add resources/VERSION.txt
+  git commit -m"'Update version to $VERSION'"
 #  echo -n "$PROPOSED_VERSION" > resources/VERSION.txt
 else
   echo "Invalid version provided: \"$PROPOSED_VERSION\". Must match semversion format of vX.X.X. Notice the \"v\" at the beginning. All values for \"X\" must be numeric"
