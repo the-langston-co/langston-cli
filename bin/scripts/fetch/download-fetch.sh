15f8fb5357ce0c6a939db2da62f96c3991e93e74
Neil Poulin <neil.j.poulin@gmail.com>
2024-01-31 17:09:39 -0700

    update langston help docs

--- bin/scripts/fetch/download-fetch.sh
+++ bin/scripts/fetch/download-fetch.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
-
-chmod +x install-gcloud.sh
-chmod +x auth-gcloud.sh
-
-./install-gcloud.sh || exit 1
-./auth-gcloud.sh || exit 1
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" || exit ; pwd -P )
+cd "$parent_path" || exit 1
+echo "Fetch current dir: $(dirname $0)"
+echo "Fetch parent_path: ${parent_path}"
+"$parent_path/scripts/fetch/install-gcloud.sh" || exit 1
+"$parent_path/scripts/fetch/auth-gcloud.sh" || exit 1
 
 echo "Finding latest version of Fetch in Google Cloud Storage"
 FETCH_ROOT="$HOME/langston-fetch"
@@ -21,8 +21,12 @@ cd "${FETCH_ROOT}" || exit 1
 
 VERSION="$(basename "$ZIP_FILENAME" .zip)"
 echo "$VERSION" > version.txt
-unzip "$FETCH_ROOT/downloads/${ZIP_FILENAME}" -d R-tmp
+rm -rf tmp
+unzip -q "$FETCH_ROOT/downloads/${ZIP_FILENAME}" -d tmp
 
 rm -rf R;
-mv R-tmp/R R
-rm -rf R-tmp
+mv tmp/R R
+rm -rf tmp
+
+echo
+echo "✅  Successfully installed version ${VERSION} of Fetch"
