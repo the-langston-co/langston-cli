6514c0e779232d0b8a7afe1edfcf57e2dec555b3
Neil Poulin <neil.j.poulin@gmail.com>
2024-02-12 22:09:52 -0700

    add warning message

--- kandji/fetch/build_image.zsh
+++ kandji/fetch/build_image.zsh
@@ -1,5 +1,12 @@
 #!/bin/zsh
 
+
+echo
+echo
+echo "***** THIS SCRIPT IS VERY FUSSY AND NOT WELL DOCUMENTED - USE WITH CAUTION ******* "
+echo
+echo
+
 mkdir -p dist
 SRC="$HOME/langston-fetch/dist"
 
@@ -7,4 +14,4 @@ SRC="$HOME/langston-fetch/dist"
 FILENAME=$(ls "$SRC" | tail -n 1)
 #FILENAME=$(find "$SRC"  -type f | tail -n 1)
 echo "Using file $FILENAME"
-#hdiutil create -volname "Fetch" -srcfolder "$SRC"  -ov -format UDZO "dist/$FILENAME"
+hdiutil create -volname "Fetch" -srcfolder "$SRC"  -ov -format UDZO "dist/$FILENAME"
